1af83a72bc1b218d3ded70b3892b99bee7453ac7
Jonah <jghebremichael95@gmail.com>
2023-01-04 19:00:59 -0500

    done

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace MethodOverloading
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace MethodOverloading
 {
     public class Program
     {
+        static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        static string Add(int num1, int num2, bool print)
+        {
+            var sum = num1 + num2;
+
+            if(print)
+            {
+                return (sum != 1 && sum != -1 ) ? $"{sum} dollars" : $"{sum} dollar";
+            }
+
+            return "";
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"{Add(1,0, true)}");
         }
+
+        
     }
 }
